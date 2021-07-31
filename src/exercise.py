28ea1c041a7cccc202faf3c3fef7f6fa66b95398
Justin Turner <jmturner638@gmail.com>
2021-07-31 01:44:07 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,17 @@
 def main():
     #write your code below this line
+    numbers = []
+
+    while True:
+        number = int(input())
+
+        if number == -1:
+            break
+
+        numbers.append(number)
+
+    for number in numbers:
+        print(number)
 
 if __name__ == '__main__':
     main()
